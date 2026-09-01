271debce07cef59d90f6a302aa447e1bcdb40f17
An Tiet <tiet.a0817@gmail.com>
2026-09-01 19:05:05 -0400

    updated alu barrel shifting issues

--- cpu/src/alu.v
+++ cpu/src/alu.v
@@ -7,7 +7,7 @@ module alu(
     output o_zero
 ); 
     assign o_zero = (o_result == 32'b0);
-    
+    wire [4:0] shift_amt = i_b[0:4]; // only first 5 bits are meaningful for 32 bit integer (0-31 bit shift)
     always @(*) begin
         case(i_alu_op)
             4'b0000 : o_result = i_a + i_b;       // ADD
@@ -15,9 +15,9 @@ module alu(
             4'b0010 : o_result = i_a ^ i_b;       // XOR
             4'b0011 : o_result = i_a | i_b;       // OR
             4'b0100 : o_result = i_a & i_b;       // AND
-            4'b0101 : o_result = i_a << i_b;      // SLL
-            4'b0110 : o_result = i_a >> i_b;      // SRL
-            4'b0111 : o_result = i_a >>> i_b;     // SRA
+            4'b0101 : o_result = i_a << shift_amt;      // SLL
+            4'b0110 : o_result = i_a >> shift_amt;      // SRL
+            4'b0111 : o_result = i_a >>> shift_amt;     // SRA
             4'b1000 : o_result = {31'b0, ($signed(i_a) < $signed(i_b))};   // SLT
             4'b1001 : o_result = {31'b0, (i_a < i_b)};                      // SLTU
         default : o_result = 32'b0;
